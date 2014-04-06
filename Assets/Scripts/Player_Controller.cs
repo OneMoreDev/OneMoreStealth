6ba8b8e411fed9a7b057fd2a1d6d06a816db262c
Pedro Lopez <pedroslopez@me.com>
2014-04-06 18:37:54 -0400

    Added a basic character controller and fixed some editor properties.

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Controller : MonoBehaviour 
+{
+	public float maxSpeed = 10f;
+	public float jumpForce = 700f;
+	public Transform groundCheck;
+	public LayerMask groundLayer;
+
+	bool facingRight = true;
+	bool isGrounded = false;
+	float groundRadius = 0.2f;
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		// JUMPING
+		if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+		{
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+		}
+	}
+	
+	void FixedUpdate()
+	{
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+
+		float move = Input.GetAxis("Horizontal");
+		
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+		if(move > 0 && !facingRight)
+			Flip();
+		else if(move < 0 && facingRight)
+			Flip();
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+
+	}
+}

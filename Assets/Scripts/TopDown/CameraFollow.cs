21cba0081f7d6715e1ca17b6c7ec1828a8b6999c
Sebastian Lawe <sebane1@thavea.com>
2014-04-06 21:56:20 -0500

    Made changes to the top view camera. Camera now follows the player.

---
+++ Assets/Scripts/TopDown/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Camera.main.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, Camera.main.transform.position.z);
+	}
+}
